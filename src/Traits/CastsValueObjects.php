bf26a9e63cb31a67d97adda9d9434e6be134cbc3
Stefano Kowalke <blueduck@mailbox.org>
2017-09-07 18:43:20 +0200

    Revert "Make the casting work"
    This reverts commit 136bcfcf19a98e74ff354bbd46abb22118f986d0.

--- src/Traits/CastsValueObjects.php
+++ src/Traits/CastsValueObjects.php
@@ -2,126 +2,58 @@
 
 namespace Waavi\ValueObjects\Traits;
 
-use Waavi\ValueObjects\ValueObject;
-
 trait CastsValueObjects
 {
-    /** @var array */
-    protected $cachedObjects = [];
-
     /**
-     * @param string $key
+     * Cast an attribute to a native PHP type.
      *
+     * @param  string  $key
+     * @param  mixed  $value
      * @return mixed
      */
-    public function getAttribute($key)
+    protected function castAttribute($key, $value)
     {
-        if (isset($this->objects) && is_array($this->objects) && isset($this->objects[$key])) {
-
-            // Allow other mutators and such to do their work first.
-            $value = parent::getAttribute($key);
-
-            // Don't cast empty $value.
-            if ($value === null || $value === '') {
-                return null;
-            }
-
-            // Cache the instantiated value for future access.
-            // This allows tests such as ($model->casted === $model->casted) to be true.
-            if (!$this->isValueObjectCached($key)) {
-                $this->cacheValueObject(
-                    $key,
-                    $this->createValueObject($key, $value)
-                );
-            }
-
-            return $this->getCachedValueObject($key);
+        if (!class_exists($this->casts[$key])) {
+            return parent::castAttribute($key, $value);
         }
 
-        return parent::getAttribute($key);
+        return new $this->casts[$key]($this->fromJson($value));
     }
 
     /**
-     * @param string $key
-     * @param mixed $value
-     */
-    public function setAttribute($key, $value)
-    {
-        if ($value instanceof ValueObject) {
-            // The value provided is a value object. We'll need to cast it to a scalar
-            // and then let it be set into Eloquent's attributes array.
-            $scalar = $value->toScalar();
-            parent::setAttribute($key, $scalar);
-
-            // Housekeeping.
-            if ($this->attributes[$key] === $scalar) {
-                // If the value wasn't modified during the set process
-                // store the original ValueObject in our cache.
-                $this->cacheValueObject($key, $value);
-            } else {
-                // Otherwise, we'll invalidate the cache for this key and defer
-                // to the get action for re-instantiating the ValueObject.
-                $this->invalidateValueObjectCache($key);
-            }
-        } elseif ($this->isValueObjectCached($key)) {
-            // This means that an attribute that has been cached to a ValueObject is being
-            // set directly as a scalar. We'll invalidate the cached ValueObject and defer
-            // to the get action for re-instantiating the ValueObject.
-            $this->invalidateValueObjectCache($key);
-            parent::setAttribute($key, $value);
-        } else {
-            // Standard value given. No need to do anything special.
-            parent::setAttribute($key, $value);
-        }
-    }
-
-    /**
-     * @param string $key
+     * Decode the given JSON back into an array or object.
      *
+     * @param  string  $value
+     * @param  bool  $asObject
      * @return mixed
      */
-    private function createValueObject($key, $value)
+    public function fromJson($value, $asObject = false)
     {
-        $class = $this->objects[$key];
-
-        return new $class($value);
+        return json_decode($value, !$asObject) ?: $value;
     }
 
     /**
-     * @param string $key
-     * @param ValueObject $object
-     */
-    private function cacheValueObject($key, ValueObject $object)
-    {
-        $this->cachedObjects[$key] = $object;
-    }
-
-    /**
-     * @param string $key
-     */
-    private function invalidateValueObjectCache($key)
-    {
-        unset($this->cachedObjects[$key]);
-    }
-
-    /**
-     * @param string $key
+     * Encode the given value as JSON.
      *
-     * @return bool
+     * @param  mixed  $value
+     * @return string
      */
-    private function isValueObjectCached($key)
+    protected function asJson($value)
     {
-        return isset($this->cachedObjects[$key]);
+        if (!$this->isValueObject($value)) {
+            return parent::asJson($value);
+        }
+        return $value->toJson();
     }
 
     /**
-     * @param string $key
+     *  Check if the given value is a ValueObject
      *
-     * @return ValueObject
+     *  @param  mixed $value
+     *  @return boolean
      */
-    private function getCachedValueObject($key)
+    protected function isValueObject($value)
     {
-        return $this->cachedObjects[$key];
+        return is_object($value) && is_subclass_of($value, ValueObject::class);
     }
-
 }
